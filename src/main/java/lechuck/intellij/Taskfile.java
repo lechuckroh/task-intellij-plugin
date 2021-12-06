cd5bfd660edd2eab48c45594328dfccf99b2e3e9
haveber <bernard.dhave@gmail.com>
2021-12-06 15:18:12 +0100

    protect the tasks field, add Unused on the setter

--- src/main/java/lechuck/intellij/Taskfile.java
+++ src/main/java/lechuck/intellij/Taskfile.java
@@ -1,14 +1,17 @@
 package lechuck.intellij;
 
+import java.util.Collections;
 import java.util.Map;
 
 public class Taskfile {
     private Map<String, Object> tasks;
 
     public Map<String, Object> getTasks() {
-        return tasks;
+        return Collections.unmodifiableMap(tasks);
     }
 
+    // TODO remove?
+    @SuppressWarnings("unused")
     public void setTasks(Map<String, Object> tasks) {
         this.tasks = tasks;
     }
