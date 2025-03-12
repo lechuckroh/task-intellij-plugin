cf038daaa950160aa4d68ba3e90d0ff91cf4d1ea
LechuckRoh <lechuckroh@gmail.com>
2025-03-12 23:14:02 +0900

    fix non-extendable methods usage violations

--- src/main/kotlin/lechuck/intellij/TaskExecutableFileChooserDescriptor.kt
+++ src/main/kotlin/lechuck/intellij/TaskExecutableFileChooserDescriptor.kt
@@ -1,18 +1,13 @@
 package lechuck.intellij
 
 import com.intellij.openapi.fileChooser.FileChooserDescriptor
-import com.intellij.openapi.vfs.VirtualFile
 
 class TaskExecutableFileChooserDescriptor : FileChooserDescriptor(true, false, false, false, false, false) {
     init {
         title = "Select Task Executable"
-    }
-
-    override fun isFileSelectable(file: VirtualFile?): Boolean {
-        if (file == null || file.isDirectory) {
-            return false
+        withFileFilter { file ->
+            val filename = file.name
+            "task".equals(filename, ignoreCase = true) || "task.exe".equals(filename, ignoreCase = true)
         }
-        val filename = file.name
-        return "task".equals(filename, ignoreCase = true) || "task.exe".equals(filename, ignoreCase = true)
     }
 }
--- src/main/kotlin/lechuck/intellij/TaskfileFileChooserDescriptor.kt
+++ src/main/kotlin/lechuck/intellij/TaskfileFileChooserDescriptor.kt
@@ -1,19 +1,13 @@
 package lechuck.intellij
 
 import com.intellij.openapi.fileChooser.FileChooserDescriptor
-import com.intellij.openapi.vfs.VirtualFile
 
 class TaskfileFileChooserDescriptor : FileChooserDescriptor(true, false, false, false, false, false) {
     init {
         title = "Select Taskfile"
-    }
-
-    override fun isFileSelectable(file: VirtualFile?): Boolean {
-        if (file == null || file.isDirectory) {
-            return false
+        withFileFilter { file ->
+            val ext = file.extension
+            "yaml".equals(ext, ignoreCase = true) || "yml".equals(ext, ignoreCase = true)
         }
-
-        val ext = file.extension
-        return "yaml".equals(ext, ignoreCase = true) || "yml".equals(ext, ignoreCase = true)
     }
 }
